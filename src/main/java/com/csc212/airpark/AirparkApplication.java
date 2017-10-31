679760080dc720af4d47b58d961a3fbd45afc4d3
Chris Dalke <chrisd@beco.io>
2017-10-31 12:52:37 -0400

    Added initial spring boot project.

---
+++ src/main/java/com/csc212/airpark/AirparkApplication.java
@@ -0,0 +1,12 @@
+package com.csc212.airpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirparkApplication.class, args);
+	}
+}

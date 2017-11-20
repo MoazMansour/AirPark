1361fb952e7b575cda7de9a407db2583331fc5aa
Roy <sroy8@rett-imac-20.ur.rochester.edu>
2017-11-20 13:32:30 -0500

    Both main pages working, error free

--- Client/AirPark For Commuter/Controller/ViewController.swift
+++ Client/AirPark For Commuter/Controller/ViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate, UISearchDisplayDelegate, UIPickerViewDelegate, UIPickerViewDataSource{
-    
+//This Class is for the HOST MODE PAGE
+class ViewControllerHostMode: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
     
     //Host Mode: Available From Field
     @IBOutlet weak var dateField: UITextField!
@@ -21,46 +21,6 @@ class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDe
     
     //UI Date Picker Object
     let picker = UIDatePicker()
-    
-    // Side-menu Leading Constraint
-    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
-    
-   //Menu View
-    @IBOutlet weak var menuView: UIView!
-    
-    //Search Bar
-    @IBOutlet weak var SearchBarMap: UISearchBar!
-    
-    //Map Code Starts
-    @IBOutlet weak var map: MKMapView!
-    let manager = CLLocationManager()
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations[0]
-        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
-        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
-        map.setRegion(region, animated: true)
-        self.map.showsUserLocation = true
-        
-        //Map Other Locations Start
-        let locations = [
-            ["title": "Wegmans Hall", "subtitle": "$2/hr", "latitude": 43.126099, "longitude": -77.629326],
-            ["title": "Morey Hall", "subtitle": "$2/hr", "latitude": 43.128394, "longitude": -77.629344],
-            ["title": "Susan B. Anthony", "subtitle": "$2/hr",  "latitude": 43.129838, "longitude": -77.626315]
-        ]
-        
-        for location in locations {
-            let annotation = MKPointAnnotation()
-            annotation.title = location["title"] as? String
-            annotation.subtitle = location["subtitle"] as? String
-            annotation.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! Double, longitude: location["longitude"] as! Double)
-            map.addAnnotation(annotation)
-        }
-        
-        //Map Other Locations End
-        
-    }
-    //Map Code Ends
     
     //Host-Mode: Available From Time Code Starts
     func createDatePicker(){
@@ -116,39 +76,6 @@ class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDe
     }
     //Host-Mode: Available To Time Code Ends
     
-    //Sliding Menu Code Start
-    var menuShowing = false
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Shadow for the side-menu
-        //menuView.layer.shadowOpacity = 2
-        //SearchBarMap.delegate = self
-        //manager.delegate=self
-        //manager.desiredAccuracy=kCLLocationAccuracyBest
-        //manager.requestWhenInUseAuthorization()
-        //manager.startUpdatingLocation()
-        createDatePicker()
-        createDatePicker2()
-        pickerView.delegate=self
-        pickerView.dataSource=self
-        textNumSpots.textAlignment = .center
-        
-        textNumSpots.inputView = pickerView
-    }
-        
-    @IBAction func openMenu(_ sender: Any) {
-        if(menuShowing){
-            leadingConstraint.constant = -140
-        } else{
-            leadingConstraint.constant = 0
-            
-            UIView.animate(withDuration: 0.3, animations: {
-                self.view.layoutIfNeeded()
-                })
-        }
-        menuShowing = !menuShowing
-    }
-    //Sliding Menu Code End
     
     //Host-Mode: Number of Spots Code Starts
     let options = ["1", "2", "3", "4"]
@@ -170,8 +97,91 @@ class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDe
         textNumSpots.text = options[row]
         textNumSpots.resignFirstResponder()
     }
+    
+    //Host-Mode: Number of Spots Code Ends
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createDatePicker()
+        createDatePicker2()
+        pickerView.delegate=self
+        pickerView.dataSource=self
+        textNumSpots.textAlignment = .center
+        
+        textNumSpots.inputView = pickerView
+    }
+    
+}
+// This Class is for the Main Page (with the MAP)
+class ViewController: UIViewController, UISearchBarDelegate, CLLocationManagerDelegate, UISearchDisplayDelegate{
+    
+    // Side-menu Leading Constraint
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
+   //Menu View
+    @IBOutlet weak var menuView: UIView!
+    
+    //Search Bar
+    @IBOutlet weak var SearchBarMap: UISearchBar!
+    
+    //Map Code Starts
+    @IBOutlet weak var map: MKMapView!
+    let manager = CLLocationManager()
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        self.map.showsUserLocation = true
+        
+        //Map Other Locations Start
+        let locations = [
+            ["title": "Wegmans Hall", "subtitle": "$2/hr", "latitude": 43.126099, "longitude": -77.629326],
+            ["title": "Morey Hall", "subtitle": "$2/hr", "latitude": 43.128394, "longitude": -77.629344],
+            ["title": "Susan B. Anthony", "subtitle": "$2/hr",  "latitude": 43.129838, "longitude": -77.626315]
+        ]
+        
+        for location in locations {
+            let annotation = MKPointAnnotation()
+            annotation.title = location["title"] as? String
+            annotation.subtitle = location["subtitle"] as? String
+            annotation.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! Double, longitude: location["longitude"] as! Double)
+            map.addAnnotation(annotation)
+        }
+        
+        //Map Other Locations End
+        
+    }
+    //Map Code Ends
 
-//Host-Mode: Number of Spots Code Ends
+    
+    //Sliding Menu Code Start
+    var menuShowing = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Shadow for the side-menu
+        menuView.layer.shadowOpacity = 2
+        SearchBarMap.delegate = self
+        manager.delegate=self
+        manager.desiredAccuracy=kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+        
+    @IBAction func openMenu(_ sender: Any) {
+        if(menuShowing){
+            leadingConstraint.constant = -140
+        } else{
+            leadingConstraint.constant = 0
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+                })
+        }
+        menuShowing = !menuShowing
+    }
+    //Sliding Menu Code End
     
     //Search Bar Home Screen Code Start
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
